07e44aa8b3bb836437fe1f7930d7d7b43e6b4856
hneutr <hunter.wapman@gmail.com>
2023-04-24 22:06:37 -0600

    20230424

--- src/hneutil/yaml.lua
+++ src/hneutil/yaml.lua
@@ -17,17 +17,24 @@ end
 function M.write_document(path, frontmatter_table, text)
     local frontmatter_str = M.dump(frontmatter_table)
 
+    text = text or ''
+
     if type(text) == "table" then
         text = string.join("\n", text)
     end
 
-    local content = frontmatter_str:rstrip() .. M.document_frontmatter_separator .. text:lstrip()
+    local content = frontmatter_str:rstrip()
+    if text:len() > 0 then
+        content = content .. M.document_frontmatter_separator .. text:lstrip()
+    end
+
     Path.write(path, content)
 end
 
 function M.read_document(path)
     local contents = Path.read(path)
     local frontmatter_str, text = unpack(contents:split("\n\n\n", 1))
+    text = text or ''
     return {M.load(frontmatter_str), text}
 end
 
